6a552c772099c08125a4d2b6fbe42855b284652f
Pavel Finagin <60171573+Wihotka@users.noreply.github.com>
2023-06-07 11:45:13 +0300

    finishScreen | fixed score display

--- src/components/modules/finishScreen/parts/userResults/UserResults.tsx
+++ src/components/modules/finishScreen/parts/userResults/UserResults.tsx
@@ -23,7 +23,6 @@ export const UserResults = ({source, finalScore, maxScore, user}:IUserResults) =
 
     const redirectUrl = source === 'platform' ? config.personalTests : config.website;
     const isFirstTime = authorized ? !enrolledOnCourse : true;
-    const tasksWithRightAnswers = tasksProgress.filter(task => task.status);
     const tasksWithWrongAnswers = tasksProgress.map((task, index) => {
         if (!task.status) return index + 1;
 
@@ -99,9 +98,9 @@ export const UserResults = ({source, finalScore, maxScore, user}:IUserResults) =
                     <path d="M137.851 10.3948C125.345 -6.50743 105.316 -0.2022 91.2544 10.9654C67.4844 29.5333 36.2151 -0.189974 12.6745 19.3446C-11.931 39.7486 2.54965 79.0914 28.3142 91.4737C80.2806 118.806 180.055 77.3993 137.851 10.3948Z" fill="#E3EEFD"/>
                 </svg>
                 <div className={styles.results}>
-                    <span>{tasksWithRightAnswers.length}</span>
+                    <span>{finalScore}</span>
                     /
-                    <span>{tasksProgress.length}</span>
+                    <span>{maxScore}</span>
                 </div>
             </div>
             <PDFDownloadLink
